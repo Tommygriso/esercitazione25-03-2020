19e173c796e56888a7f039557891c9c972cc3ea1
Tommaso Grisanti <tomtommy04@gmail.com>
2020-03-25 14:57:19 +0100

    aggiunto codice python

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ if age > 18:
 	print("sei maggiorenne grande bro")
 if age < 18 :
 	print("sei un cinno inutile")
+color = input("quale è il tuo colore preferito?")
+print ("il tuo colore preferito è",color)
+print ("quindi fammi capire, ti chiami",nome,"hai",age,"anni")
+
